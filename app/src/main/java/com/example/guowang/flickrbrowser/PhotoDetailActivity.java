b46bbddaa81b95053b032bb26db67abf6d290511
guowang <guowang94@outlook.com>
2019-01-28 20:54:03 +0800

    Commit Project to git

---
+++ app/src/main/java/com/example/guowang/flickrbrowser/PhotoDetailActivity.java
@@ -0,0 +1,39 @@
+package com.example.guowang.flickrbrowser;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class PhotoDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_detail);
+
+        activateToolbar(true);
+
+        Intent intent = getIntent();
+        Photo photo = (Photo) intent.getSerializableExtra(PHOTO_TRANSFER);
+        if (photo != null) {
+            TextView photoTitle = findViewById(R.id.photo_title);
+            photoTitle.setText("Title: " + photo.getTitle());
+
+            TextView photoTags = findViewById(R.id.photo_tags);
+            photoTags.setText("Tags: " + photo.getTags());
+
+            TextView photoAuthor = findViewById(R.id.photo_author);
+            photoAuthor.setText(photo.getAuthor());
+
+            ImageView photoImage = findViewById(R.id.photo_image);
+            Picasso.get().load(photo.getLink())
+                    .error(R.drawable.baseline_image_black_48)
+                    .placeholder(R.drawable.baseline_image_black_48)
+                    .into(photoImage);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/guowang/flickrbrowser/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.example.guowang.flickrbrowser;
+
+import android.os.Bundle;
+
+public class SearchActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        activateToolbar(true);
+    }
+
+}
